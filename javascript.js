5ec3efb58f65305f0cf8b8f8826c6b09e3a1e607
Juan Carlos Jaramillo Vásquez <juan.jaramillov@ucuenca.ec>
2015-09-25 13:16:15 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+/*
+Copyright (c) 2015 Jaramillo Juan Carlos, Gallardo Rafael
+
+        coberturaWerb is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+        
+        coberturaWerb is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+        
+        You should have received a copy of the GNU General Public License
+        along with coberturaWerb.  If not, see <http://www.gnu.org/licenses/>.*/
+
 google.load("visualization", "0", {packages:["corechart"]});
     google.load('visualization', '1', {packages: ['corechart', 'line']});
                 var map = null;
